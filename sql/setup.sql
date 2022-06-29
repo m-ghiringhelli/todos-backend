0e16ce8fc545b65ec34492fc5d8352f5f5e86e36
m-ghiringhelli <mghiring@gmail.com>
2022-06-28 17:41:14 -0700

    created user table in sql

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,7 @@
--- Use this file to define your SQL tables
--- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  email VARCHAR NOT NULL UNIQUE,
+  password_hash VARCHAR NOT NULL
+);
