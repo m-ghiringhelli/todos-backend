e72eeabf0c6901d9fd9af3f716f88df7aa1b5a4b
m-ghiringhelli <mghiring@gmail.com>
2022-07-10 15:07:20 -0700

    created create user test

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -3,12 +3,26 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('users', ()  => {
+const mockUser = {
+  email: 'test@test.com',
+  password: '123123'
+};
+
+describe('users', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  
+  it('creates a new user', async () => {
+    const res = await request(app).post('/api/v1/users').send(mockUser);
+    const { email } = mockUser;
+
+    expect(res.status).toEqual(200);
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email
+    });
+  });
 
   afterAll(() => {
     pool.end();
