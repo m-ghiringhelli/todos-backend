00b9422c9533c09fbddcff7e4c66a410ad7bb185
m-ghiringhelli <mghiring@gmail.com>
2022-07-11 10:11:15 -0700

    test created for returning current user

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -2,10 +2,23 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 const mockUser = {
   email: 'test@test.com',
   password: '123123'
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+
+  const agent = request.agent(app);
+
+  const user = await UserService.create({ ...mockUser, ...userProps });
+
+  const { email } = user;
+  await agent.post('/api/vi/users/sessions').send({ email, password });
+  return [agent, user];
 };
 
 describe('users', () => {
@@ -21,6 +34,16 @@ describe('users', () => {
     expect(res.body).toEqual({
       id: expect.any(String),
       email
+    });
+  });
+
+  it('returns the current user', async () => {
+    const [agent, user] = await registerAndLogin();
+    const me = await agent.get('/api/v1/users/me');
+    expect(me.body).toEqual({
+      ...user,
+      exp: expect.any(Number),
+      iat: expect.any(Number)
     });
   });
 
